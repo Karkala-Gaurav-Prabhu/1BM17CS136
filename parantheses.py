3a345b701165a99b49ea310b0151eee1ac677049
Karkala Gaurav Prabhu <43137056+Karkala-Gaurav-Prabhu@users.noreply.github.com>
2019-10-10 12:07:20 +0530

    Add files via upload

---
+++ parantheses.py
@@ -0,0 +1,29 @@
+class Validate:
+     def __init__(self, string):
+          self.string = string
+
+     def isvalid(self):
+          count = 0
+          for parantheses in string:
+               if parantheses == '(':
+                    count += 1
+               elif parantheses == ')':
+                    count -= 1
+               elif parantheses == '{':
+                    count += 2
+               elif parantheses == '}':
+                    count -= 2
+               elif parantheses == '[':
+                    count += 3
+               elif parantheses == ']':
+                    count -= 3
+
+          if count == 0 :
+               print("Valid")
+          else:
+               print("Invalid")
+
+string = input("Enter A String Of Parantheses")
+
+val = Validate(string)
+val.isvalid()
